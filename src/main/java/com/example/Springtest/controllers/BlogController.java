db8d940659c529855f5b48549d2a54d9869ea5d6
Viacheslav Fedchenko <viacheslav.fedchenko@piano.io>
2022-02-24 20:45:17 +0300

    add detailed post page

--- src/main/java/com/example/Springtest/controllers/BlogController.java
+++ src/main/java/com/example/Springtest/controllers/BlogController.java
@@ -3,11 +3,16 @@ package com.example.Springtest.controllers;
 import com.example.Springtest.models.Post;
 import com.example.Springtest.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -36,5 +41,17 @@ public class BlogController {
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String fullBlog(@PathVariable(value = "id") long id, Model model){
+        if(!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-details";
     }
 }
