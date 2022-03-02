b723045c6f4e78a0b23e60177997858e7e0797ae
Viacheslav Fedchenko <viacheslav.fedchenko@piano.io>
2022-03-02 22:17:41 +0300

    add edition and removing of posts

--- src/main/java/com/example/Springtest/controllers/BlogController.java
+++ src/main/java/com/example/Springtest/controllers/BlogController.java
@@ -3,7 +3,6 @@ package com.example.Springtest.controllers;
 import com.example.Springtest.models.Post;
 import com.example.Springtest.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,31 +20,30 @@ public class BlogController {
     private PostRepository postRepository;
 
     @GetMapping("/blog")
-    public String blogMain(Model model){
+    public String blogMain(Model model) {
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
         return "blog-main";
     }
 
     @GetMapping("/blog/add")
-    public String blogAdd(Model model){
+    public String blogAdd(Model model) {
         return "blog-add";
     }
 
     @PostMapping("/blog/add")
-
     public String newPostAdd(@RequestParam String title,
                              @RequestParam String anons,
                              @RequestParam String full_text,
-                             Model model){
+                             Model model) {
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
         return "redirect:/blog";
     }
 
     @GetMapping("/blog/{id}")
-    public String fullBlog(@PathVariable(value = "id") long id, Model model){
-        if(!postRepository.existsById(id)) {
+    public String fullBlog(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
             return "redirect:/blog";
         }
         Optional<Post> post = postRepository.findById(id);
@@ -53,5 +51,38 @@ public class BlogController {
         post.ifPresent(result::add);
         model.addAttribute("post", result);
         return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String newPostUpdate(@PathVariable(value = "id") long id,
+                                @RequestParam String title,
+                                @RequestParam String anons,
+                                @RequestParam String full_text,
+                                Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/remove")
+    public String newPostRemove(@PathVariable(value = "id") long id, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+        return "redirect:/blog";
     }
 }
